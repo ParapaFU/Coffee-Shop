18ecf6512e20b34da1c23b3aa5f5efcd0f45aec6
Parapa <parapasndy@gmail.com>
2026-04-02 23:56:02 +0700

    fix, animasi, dan beberapa fitur

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@
 const header = document.querySelector("header");
 // hero
 const hero = document.querySelector(".hero");
+// fade up
+const fadeUp = document.querySelectorAll(".fade-up");
+// fade left
+const fadeLeft = document.querySelectorAll(".fade-left");
+// fade right
+const fadeRight = document.querySelectorAll(".fade-right");
 
 // Function
 // responsive hero dan header
@@ -11,8 +17,25 @@ function autoHeightHeader() {
   hero.style.setProperty("--hero-height", heightHeader + "px");
 }
 
+// sistem deteksi fade up
+const observer = new IntersectionObserver(
+  (enteries) => {
+    enteries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+
 // Event
 window.addEventListener("resize", autoHeightHeader);
 
 // Program
 autoHeightHeader();
+fadeUp.forEach((e) => observer.observe(e));
+fadeLeft.forEach((e) => observer.observe(e));
+fadeRight.forEach((e) => observer.observe(e));
